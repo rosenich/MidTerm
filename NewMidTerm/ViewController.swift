eba1119227d5cf2bee91a14dc51eb79b58e905c2
firstcommit <rosenich@badinhs.org>
2017-01-06 08:23:13 -0500

    first commit

--- NewMidTerm/ViewController.swift
+++ NewMidTerm/ViewController.swift
@@ -4,20 +4,57 @@
 //
 //  Created by BHSRam5 on 1/5/17.
 //  Copyright © 2017 BHSRam5. All rights reserved.
-//
+
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController:
+    UIViewController ,
+    UITableViewDataSource ,
+    UITableViewDelegate{
+    
+    @IBOutlet weak var sportsTableView: UITableView!
+    
+    
+    
+    var sports=["Fall", "Winter", "Spring"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        sportsTableView.dataSource=self
+        
+        sportsTableView.delegate=self
+
+        
     }
 
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sports.count
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print(indexPath.row)
+        let cell=UITableViewCell()
+        cell.textLabel?.text=sports[indexPath.row]
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "moveSegue", sender: "Fall")
+    }
+
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+            // Dispose of any resources that can be recreated.
     }
 
 
